92bacd9411bbcb5631f549773ab87cbdb6fd3f87
Sven Budak <sven@budak.info>
2020-04-18 15:08:38 +0200

    initial commit

--- projects/mark6-lib/src/public-api.ts
+++ projects/mark6-lib/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of mark6-lib
  */
 
-export * from './lib/mark6-lib.service';
-export * from './lib/mark6-lib.component';
-export * from './lib/mark6-lib.module';
+// export * from './lib/mark6-lib.service';
+// export * from './lib/mark6-lib.component';
+// export * from './lib/mark6-lib.module';
+
+export * from './lib/image/image.module';
