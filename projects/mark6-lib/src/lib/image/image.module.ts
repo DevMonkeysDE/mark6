5bccdad40647263f3f5185a0d775f2fa26cbd652
Sven Budak <sven@budak.info>
2020-04-18 14:47:46 +0200

    initial commit

---
+++ projects/mark6-lib/src/lib/image/image.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Mark6ImageComponent} from './image.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [Mark6ImageComponent],
+  declarations: [Mark6ImageComponent]
+})
+export class Mark6ImageModule {
+}
