91ad3eb6684ac3fce350a664970ef934394cd00f
Sven Budak <sven@budak.info>
2020-04-26 13:02:24 +0200

    migrate from ngx-mark6

---
+++ projects/mark6-lib/src/lib/drop-down/drop-down.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+    Mark6DropDownComponent,
+    Mark6DropDownContentComponent,
+    Mark6DropDownOriginComponent,
+    Mark6DropDownTriggerComponent,
+    Mark6DropDownCloseTriggerDirective
+} from './drop-down.component';
+import {Mark6DropDownService} from './drop-down.service';
+
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        Mark6DropDownComponent,
+        Mark6DropDownTriggerComponent,
+        Mark6DropDownOriginComponent,
+        Mark6DropDownContentComponent,
+        Mark6DropDownCloseTriggerDirective
+    ],
+    declarations: [
+        Mark6DropDownComponent,
+        Mark6DropDownTriggerComponent,
+        Mark6DropDownOriginComponent,
+        Mark6DropDownContentComponent,
+        Mark6DropDownCloseTriggerDirective
+    ],
+    providers: [
+        Mark6DropDownService
+    ]
+})
+
+
+export class Mark6DropDownModule {
+}
---
+++ projects/mark6-lib/src/lib/drop-down/drop-down.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Observable, Subject} from 'rxjs'; // Observable is required because build task throws otherwise errors.
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Mark6DropDownService {
+
+    // Observable string sources
+    private subject = new Subject();
+
+    close(target) {
+        this.subject.next(target);
+    }
+
+    getState() {
+        return this.subject.asObservable();
+    }
+
+}
