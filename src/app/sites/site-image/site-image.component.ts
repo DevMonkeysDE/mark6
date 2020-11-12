497162e902e517ccc3f28682ad43a52805dd791f
Sven Budak <sven@budak.info>
2020-11-12 14:48:41 +0100

    migrate from ngx-mark6

--- src/app/sites/site-image/site-image.component.ts
+++ src/app/sites/site-image/site-image.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { of } from 'rxjs';
-import { delay } from 'rxjs/operators';
-
 
 @Component({
     selector: 'app-site-image',
@@ -9,15 +6,7 @@ import { delay } from 'rxjs/operators';
     styleUrls: ['./site-image.component.scss']
 })
 
-
 export class SiteImageComponent {
-
-    srcSet = of(`https://via.placeholder.com/300x150 300w,
-    https://via.placeholder.com/600x300 600w,
-    https://via.placeholder.com/960x480 960w,
-    https://via.placeholder.com/1400x700 1400w`).pipe(
-        delay(5000)
-    );
 
     public dummy = {
         avatar: 'assets/demo/avatar-female-asian.jpg',
@@ -25,8 +14,5 @@ export class SiteImageComponent {
         demo: 'assets/demo/demo.jpg',
         alt: 'Alt Text',
     };
-
-    constructor() {
-    }
 
 }
