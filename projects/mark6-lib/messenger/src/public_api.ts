595bb03af32c8c85c8c977136666ff8b0da3717e
Sven Budak <sven@budak.info>
2020-11-17 14:18:34 +0100

    migrate from ngx-mark6

--- projects/mark6-lib/messenger/src/public_api.ts
+++ projects/mark6-lib/messenger/src/public_api.ts
@@ -4,9 +4,9 @@
  */
 
 export * from './messenger.module';
+export * from './messenger.component';
 export * from './messenger-content.component';
-export * from './messenger-emote-picker.component';
-export * from './messenger-footer.component';
 export * from './messenger-header.component';
 export * from './messenger-messages.component';
-export * from './messenger.component';
+export * from './messenger-footer.component';
+export * from './messenger-emote-picker.component';
