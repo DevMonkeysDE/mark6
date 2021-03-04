5d1d7f997324813788e6b9e288ea0196fc626cb9
Alex <faustino.alexandre@outlook.com>
2021-03-04 17:11:59 +0100

    autoSize

--- projects/mark6-lib/messenger/src/messenger-textarea-auto-size.directive.ts
+++ projects/mark6-lib/messenger/src/messenger-textarea-auto-size.directive.ts
@@ -1,18 +1,17 @@
-import {AfterViewInit, Directive, ElementRef, HostListener} from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 
 @Directive({
-    selector: 'textarea[Mark6MessengerAutoSize]',
-    // TODO :host is deprecated. solve it in the modern way
-    host: {
-        'rows': '1',
-        'style': 'overflow: hidden'
-    }
+    selector: 'textarea[Mark6MessengerAutoSize]'
 })
 export class Mark6MessengerAutoSizeDirective implements AfterViewInit {
 
+    @Input() maxHeight = 200;
     // TODO ALEX - Please add Max count for row Auto grow
     constructor(private elem: ElementRef) {
+        elem.nativeElement.style.overflow = 'hidden';
+        elem.nativeElement.rows = '1';
+
     }
 
     public ngAfterViewInit() {
@@ -23,8 +22,10 @@ export class Mark6MessengerAutoSizeDirective implements AfterViewInit {
     public resize() {
         const textArea = this.elem.nativeElement as HTMLTextAreaElement;
         // Reset textArea height to auto that correctly calculate the new height
-        textArea.style.height = 'auto';
         // Set new height
-        textArea.style.height = `${textArea.scrollHeight}px`;
+        if (textArea.scrollHeight < this.maxHeight) {
+            textArea.style.height = 'auto';
+            textArea.style.height = `${textArea.scrollHeight}px`;
+        }
     }
 }
