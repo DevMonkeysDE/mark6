159071e20f2be46b678a193fc0df2f5df10e94cc
Sven Budak <sven@budak.info>
2021-07-01 10:11:54 +0200

    mark6-messenger-auto-expand removed

--- projects/mark6-lib/services/src/preferred-color-scheme.service.ts
+++ projects/mark6-lib/services/src/preferred-color-scheme.service.ts
@@ -10,7 +10,7 @@ export class Mark6PreferredColorSchemeService {
 
     private renderer: Renderer2;
     private colorScheme: string;
-    // Define prefix for more clear and readable styling classes in scss files
+    // Define prefix for clearer and more readable class names in scss files
     private colorSchemePrefix = 'color-scheme-';
 
     constructor(rendererFactory: RendererFactory2) {
@@ -21,27 +21,28 @@ export class Mark6PreferredColorSchemeService {
     _detectPrefersColorScheme() {
         // Detect if prefers-color-scheme is supported
         if (window.matchMedia('(prefers-color-scheme)').media !== 'not all') {
-            // Set colorScheme to Dark if prefers-color-scheme is dark. Otherwise set to light.
+            // Set colorScheme to Dark if prefers-color-scheme is dark. Otherwise, set it to Light.
             this.colorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
         } else {
-            // If browser dont support prefers-color-scheme, set it as default to dark
+            // If the browser does not support prefers-color-scheme, set the default to dark.
             this.colorScheme = 'dark';
         }
     }
 
-    _setColorScheme(scheme) {
+    _setColorScheme(scheme: string) {
         this.colorScheme = scheme;
         // Save prefers-color-scheme to localStorage
         localStorage.setItem('prefers-color', scheme);
     }
 
     _getColorScheme() {
+        const localStorageColorScheme = localStorage.getItem('prefers-color');
         // Check if any prefers-color-scheme is stored in localStorage
-        if (localStorage.getItem('prefers-color')) {
+        if (localStorageColorScheme) {
             // Save prefers-color-scheme from localStorage
-            this.colorScheme = localStorage.getItem('prefers-color');
+            this.colorScheme = localStorageColorScheme;
         } else {
-            // If no prefers-color-scheme is stored in localStorage, Try to detect OS default prefers-color-scheme
+            // If no prefers-color-scheme is stored in localStorage, try to detect OS default prefers-color-scheme
             this._detectPrefersColorScheme();
         }
     }
@@ -51,7 +52,7 @@ export class Mark6PreferredColorSchemeService {
         this.renderer.addClass(document.body, this.colorSchemePrefix + this.colorScheme);
     }
 
-    update(scheme) {
+    update(scheme: string) {
         this._setColorScheme(scheme);
         // Remove the old color-scheme class
         this.renderer.removeClass(document.body, this.colorSchemePrefix + (this.colorScheme === 'dark' ? 'light' : 'dark'));
